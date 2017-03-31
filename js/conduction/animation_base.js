0948570036d3c23e62f30321e2f033b0ef2d30b6
Laszlo Boros <iamsemmu@gmail.com>
2017-04-01 00:13:40 +0200

    WIP animation base and specialization added

---
+++ js/conduction/animation_base.js
@@ -0,0 +1,15 @@
+define({
+    name: "Animation Name",
+
+    onLoad: function() {
+        throw new Error("missing onLoad");
+    },
+
+    requestAnimationFrame: function() {
+        throw new Error("missing requestAnimationFrame");
+    },
+
+    onUnload: function() {
+        throw new Error("missing onUnload");
+    }
+});
---
+++ js/conduction/animations/PN.js
@@ -0,0 +1,10 @@
+define(['../animation_base'], function(animation) {
+
+    animation.name = "PN anim";
+
+    animation.onLoad = function() {
+        console.log("PN onLoad");
+    }
+
+    return animation;
+});
