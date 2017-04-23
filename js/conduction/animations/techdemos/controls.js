b2edb5e2511abdd19fe22a5265a68e35fa303752
Laszlo Boros <iamsemmu@gmail.com>
2017-04-23 20:25:40 +0200

    some code reorganize

---
+++ js/conduction/animations/techdemos/controls.js
@@ -0,0 +1,74 @@
+define(['../../animation_base', '../../controls'], function(animation, controls) {
+
+    animation.name = "PN diode";
+    animation.description = "A simple animation showing the PN diode and its behaviour.";
+
+    animation.onButtonClicked = function() {
+        alert('müxik');
+    }
+
+    animation.onRender = function() {
+        console.log("do stuff");
+    }
+
+    animation.onLoad = function() {
+        var top_rect = new PIXI.Graphics();
+
+        top_rect.lineStyle(0, 0x000000, 0);
+        top_rect.beginFill(0xff0000);
+        top_rect.drawRect(-200, -20, 150, 40);
+        top_rect.endFill();
+
+        top_rect.beginFill(0x0000ff);
+        top_rect.drawRect(50, -20, 150, 40);
+        top_rect.endFill();
+
+        top_rect.lineStyle(2, 0x000000, 1);
+        top_rect.drawRect(-200, -20, 400, 40);
+        top_rect.moveTo(0, -20);
+        top_rect.lineStyle(1, 0xdddd00, 1);
+        top_rect.lineTo(0, 20);
+
+        animation.scene.addChild(top_rect);
+    }
+
+    animation.settings = [
+        controls.Text('Gombok:'),
+        controls.Button('gomb 1', function() {alert('btn1');}),
+        controls.Button('bomg 2', function() {alert('btn 2');}),
+        controls.Divider(),
+        controls.Text('Csekbokszok:'),
+        controls.Checkbox('csekk it', true, function(state){alert('elso ' + state);}),
+        controls.Checkbox('csekk ezt is', true, function(state){alert('2: ' + state);}),
+        controls.Divider(),
+        controls.Text('Szólarádiók:'),
+        controls.Radio([
+            { label: 'első', value: 'elso' },
+            { label: 'HÁROM', value: 'tevagyazen' },
+        ], function(val) {alert('r1:' + val);}),
+        controls.Radio([
+            { label: 'a', value: '1' },
+            { label: 'b', value: '2' },
+            { label: 'c', value: '3' },
+            { label: 'd', value: '4' },
+            { label: 'e', value: '5' }
+        ], function(val) {alert('r2: ' + val);}),
+        controls.Divider(),
+        controls.Text('Réndzsek:'),
+        controls.Range(5, 0, 1, 10, function(val) { alert('range1: ' + val);}),
+        controls.Range(5, -5, 0.1, 15, function(val) { alert('range 2: ' + val);}),
+        controls.Divider(),
+        controls.Text('Námbörsz:'),
+        controls.Number(5, 0, 1, 10, function(val) { alert('num 1: ' + val);}),
+        controls.Number(5, -5, 0.1, 15, function(val) { alert('num 2: ' + val);}),
+        controls.Divider(),
+        controls.Text('Szelektek'),
+        controls.Select([
+            {text: 'Első', value: 'elso'},
+            {text: 'Második', value: 'tu'},
+            {text: '3', value: 'tre'},
+        ], function(val) {alert(val);})
+    ];
+
+    return animation;
+});
---
+++ js/conduction/animations/techdemos/cubegrid.js
@@ -0,0 +1,281 @@
+define(['../../util', '../../animation_base', '../../controls', '../../3D'], function(util, animation, controls, ddd) {
+
+    animation.name = "Cube Grid";
+    animation.description = "This techdemo shows a simple cubegrid created with spheres and lines connecting them.\n\nThe object can be freely rotated by dragging it.\n\nYou can toggle the auto rotation and change the viewing distance of the cube with the controls below.";
+
+    var Spheres = [];
+    animation.autoRotate = true;
+    var objectsToDraw = [];
+
+    var Grid = {
+        Position: ddd.Vector(0, 0, 0),
+        Axes: {
+            x: ddd.Vector(1, 0, 0),
+            y: ddd.Vector(0, 1, 0),
+            z: ddd.Vector(0, 0, 1)
+        },
+        rotate: function(flat, lean) {
+            Grid.Axes.x = Grid.Axes.x.rotate(ddd.Vector(0, 1, 0), flat).rotate(ddd.Vector(1, 0, 0), lean).setLength(1);
+            Grid.Axes.y = Grid.Axes.y.rotate(ddd.Vector(0, 1, 0), flat).rotate(ddd.Vector(1, 0, 0), lean).setLength(1);
+            Grid.Axes.z = Grid.Axes.z.rotate(ddd.Vector(0, 1, 0), flat).rotate(ddd.Vector(1, 0, 0), lean).setLength(1);
+        }
+    };
+
+    var SPHERE_SIZE = 50;
+    var SPHERE_GAP = 80;
+
+    var Sphere = function(x, y, z) {
+
+        var Sphere = {
+            Graphics: new PIXI.Graphics(),
+            Position: ddd.Vector(x, y, z),
+            Color: util.randInt(0xffffff),
+
+            getAbsolutePosition: function() {
+                return Grid.Position.add(
+                    Sphere.Position.transform(
+                        Grid.Axes.x,
+                        Grid.Axes.y,
+                        Grid.Axes.z
+                ));
+            },
+
+            distanceFromCamera: null,
+
+            computeDistanceFromCamera: function() {
+                Sphere.distanceFromCamera = Sphere.getAbsolutePosition().distanceFromCamera();
+            },
+
+            zDistance: null,
+
+            computeZDistance: function() {
+                Sphere.zDistance = Sphere.getAbsolutePosition().z;
+            },
+
+            draw: function() {
+                Sphere.Graphics.clear();
+
+                var absolutePosition = Sphere.getAbsolutePosition();
+                var projection = absolutePosition.getProjection();
+
+                Sphere.Graphics.beginFill(Sphere.Color);
+                Sphere.Graphics.lineStyle(2, 0x000000, 1);
+                Sphere.Graphics.drawCircle(projection.x, projection.y, ddd.getProjectedDistance(SPHERE_SIZE, absolutePosition.distanceFromCamera()));
+                Sphere.Graphics.endFill();
+            }
+        };
+
+        Sphere.draw();
+
+        return Sphere;
+    }
+
+    var Line = function(from, to) {
+
+        var Line = {
+            Graphics: new PIXI.Graphics(),
+            From: ddd.Vector(from.x, from.y, from.z),
+            To: ddd.Vector(to.x, to.y, to.z),
+
+            getAbsolutePosition: function() {
+                return {
+                    From: Grid.Position.add(Line.From.transform(Grid.Axes.x, Grid.Axes.y, Grid.Axes.z)),
+                    To: Grid.Position.add(Line.To.transform(Grid.Axes.x, Grid.Axes.y, Grid.Axes.z))
+                };
+            },
+
+            distanceFromCamera: null,
+
+            computeDistanceFromCamera: function() {
+                var absolutePosition = Line.getAbsolutePosition();
+                var middle = absolutePosition.From.add(absolutePosition.To).scale(0.5);
+                Line.distanceFromCamera = middle.distanceFromCamera();
+            },
+
+            zDistance: null,
+
+            computeZDistance: function() {
+                var absolutePosition = Line.getAbsolutePosition();
+                Line.zDistance = (absolutePosition.From.z + absolutePosition.To.z) / 2;
+            },
+
+            draw: function() {
+                var absolutePosition = Line.getAbsolutePosition();
+                Line.Graphics.clear();
+                Line.Graphics.moveTo(absolutePosition.From.getProjection().x, absolutePosition.From.getProjection().y);
+                Line.Graphics.lineStyle(2, 0x000000, 1);
+                Line.Graphics.lineTo(absolutePosition.To.getProjection().x, absolutePosition.To.getProjection().y);
+            }
+        };
+
+        Line.draw();
+
+        return Line;
+
+    }
+
+    var draggableOverlay = new PIXI.Graphics();
+
+    animation.onLoad = function() {
+        draggableOverlay.beginFill(0xf6f5a4);
+        draggableOverlay.drawRect(ddd.Camera.WIDTH / -2, ddd.Camera.HEIGHT / -2, ddd.Camera.WIDTH, ddd.Camera.HEIGHT);
+        draggableOverlay.endFill();
+        draggableOverlay.interactive = true;
+        draggableOverlay.alpha = 0;
+
+        draggableOverlay.onDragStart = function(event) {
+            this.data = event.data;
+            this.data.autoRotate = animation.autoRotate;
+            animation.autoRotate = false;
+
+            this.dragging = true;
+            this.dragStartPos = event.data.getLocalPosition(this.parent);
+
+            this.data.previousPosition = {
+                x: this.x,
+                y: this.y
+            }
+        }
+
+        draggableOverlay.onDragMove = function() {
+            if (this.dragging) {
+                var newPosition = this.data.getLocalPosition(this.parent);
+                this.x = newPosition.x - this.dragStartPos.x;
+                this.y = newPosition.y - this.dragStartPos.y;
+
+                Grid.rotate((this.data.previousPosition.x - this.x) / 100, (this.data.previousPosition.y - this.y) / 100);
+
+                this.data.previousPosition = {
+                    x: this.x,
+                    y: this.y
+                };
+            }
+        }
+
+        draggableOverlay.onDragEnd = function() {
+            animation.autoRotate = this.data.autoRotate;
+            this.dragging = false;
+            this.data = null;
+            this.x = 0;
+            this.y = 0;
+        }
+
+        draggableOverlay.on('pointerdown', draggableOverlay.onDragStart)
+        .on('pointerup', draggableOverlay.onDragEnd)
+        .on('pointermove', draggableOverlay.onDragMove);
+
+        animation.scene.addChild(draggableOverlay);
+
+        var SIZE = 3;
+        for (var x = 0; x < SIZE; x++) {
+            for (var y = 0; y < SIZE; y++) {
+                for (var z = 0; z < SIZE; z++) {
+                    var s = Sphere(
+                        (x+1) * SPHERE_GAP - (0.5 * SIZE * SPHERE_GAP) - (0.5 * SPHERE_GAP),
+                        (y+1) * SPHERE_GAP - (0.5 * SIZE * SPHERE_GAP) - (0.5 * SPHERE_GAP),
+                        (z+1) * SPHERE_GAP - (0.5 * SIZE * SPHERE_GAP) - (0.5 * SPHERE_GAP)
+                    );
+                    objectsToDraw.push(s);
+                    animation.scene.addChild(s.Graphics);
+
+                    if (x != SIZE - 1)
+                    {
+                        var lx = Line(
+                            ddd.Vector(
+                                (x+1 + (SPHERE_SIZE/2) / SPHERE_GAP) * SPHERE_GAP - (0.5 * SIZE * SPHERE_GAP) - (0.5 * SPHERE_GAP),
+                                (y+1) * SPHERE_GAP - (0.5 * SIZE * SPHERE_GAP) - (0.5 * SPHERE_GAP),
+                                (z+1) * SPHERE_GAP - (0.5 * SIZE * SPHERE_GAP) - (0.5 * SPHERE_GAP)
+                            ),
+                            ddd.Vector(
+                                (x+2 - (SPHERE_SIZE/2) / SPHERE_GAP) * SPHERE_GAP - (0.5 * SIZE * SPHERE_GAP) - (0.5 * SPHERE_GAP),
+                                (y+1) * SPHERE_GAP - (0.5 * SIZE * SPHERE_GAP) - (0.5 * SPHERE_GAP),
+                                (z+1) * SPHERE_GAP - (0.5 * SIZE * SPHERE_GAP) - (0.5 * SPHERE_GAP)
+                            )
+                        );
+
+                        objectsToDraw.push(lx);
+                        animation.scene.addChild(lx.Graphics);
+                    }
+
+                    if (y != SIZE - 1)
+                    {
+                        var ly = Line(
+                            ddd.Vector(
+                                (x+1) * SPHERE_GAP - (0.5 * SIZE * SPHERE_GAP) - (0.5 * SPHERE_GAP),
+                                (y+1 + (SPHERE_SIZE/2) / SPHERE_GAP) * SPHERE_GAP - (0.5 * SIZE * SPHERE_GAP) - (0.5 * SPHERE_GAP),
+                                (z+1) * SPHERE_GAP - (0.5 * SIZE * SPHERE_GAP) - (0.5 * SPHERE_GAP)
+                            ),
+                            ddd.Vector(
+                                (x+1) * SPHERE_GAP - (0.5 * SIZE * SPHERE_GAP) - (0.5 * SPHERE_GAP),
+                                (y+2 - (SPHERE_SIZE/2) / SPHERE_GAP) * SPHERE_GAP - (0.5 * SIZE * SPHERE_GAP) - (0.5 * SPHERE_GAP),
+                                (z+1) * SPHERE_GAP - (0.5 * SIZE * SPHERE_GAP) - (0.5 * SPHERE_GAP)
+                            )
+                        );
+
+                        objectsToDraw.push(ly);
+                        animation.scene.addChild(ly.Graphics);
+                    }
+
+                    if (z != SIZE - 1)
+                    {
+                        var lz = Line(
+                            ddd.Vector(
+                                (x+1) * SPHERE_GAP - (0.5 * SIZE * SPHERE_GAP) - (0.5 * SPHERE_GAP),
+                                (y+1) * SPHERE_GAP - (0.5 * SIZE * SPHERE_GAP) - (0.5 * SPHERE_GAP),
+                                (z+1 + (SPHERE_SIZE/2) / SPHERE_GAP) * SPHERE_GAP - (0.5 * SIZE * SPHERE_GAP) - (0.5 * SPHERE_GAP)
+                            ),
+                            ddd.Vector(
+                                (x+1) * SPHERE_GAP - (0.5 * SIZE * SPHERE_GAP) - (0.5 * SPHERE_GAP),
+                                (y+1) * SPHERE_GAP - (0.5 * SIZE * SPHERE_GAP) - (0.5 * SPHERE_GAP),
+                                (z+2 - (SPHERE_SIZE/2) / SPHERE_GAP) * SPHERE_GAP - (0.5 * SIZE * SPHERE_GAP) - (0.5 * SPHERE_GAP)
+                            )
+                        );
+
+                        objectsToDraw.push(lz);
+                        animation.scene.addChild(lz.Graphics);
+                    }
+                }
+            }
+        }
+    }
+
+    var renderStart, renderEnd;
+
+    animation.onRender = function() {
+
+        renderStart = performance.now();
+
+        animation.scene.removeChildren();
+
+        if (animation.autoRotate)
+            Grid.rotate(0.003, 0);
+
+
+        for (var i = objectsToDraw.length - 1; i >= 0; i--) {
+            objectsToDraw[i].computeZDistance();
+        }
+
+        objectsToDraw.sort(function(a, b) {
+            return a.zDistance - b.zDistance;
+        });
+
+        for (var i = objectsToDraw.length - 1; i >= 0; i--) {
+            objectsToDraw[i].draw();
+            animation.scene.addChild(objectsToDraw[i].Graphics);
+        }
+
+        animation.scene.addChild(draggableOverlay);
+
+        renderEnd = performance.now();
+        //console.log(renderEnd - renderStart);
+    }
+
+    animation.settings = [
+        controls.Text('Rotation'),
+        controls.Checkbox('Auto rotation', animation.autoRotate, function() {animation.autoRotate = !animation.autoRotate;}),
+        controls.Text('Distance'),
+        controls.Range(Grid.Position.z, -100, 1, 500, function(val) {Grid.Position.z=val;})
+    ];
+
+    return animation;
+});
