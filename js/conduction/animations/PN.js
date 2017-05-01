db305f83f2a937afb5da3d024b62bdeb6108c139
Laszlo Boros <iamsemmu@gmail.com>
2017-05-02 01:40:52 +0200

    PN diode graphics WIP

--- js/conduction/animations/PN.js
+++ js/conduction/animations/PN.js
@@ -5,6 +5,7 @@ define(['../animation_base', '../controls'], function(animation, controls) {
 
     var settings = {
         voltage: 0.0,
+        fillRatio: 0.6,
 
         leakage: true,
         injection: true,
@@ -39,6 +40,115 @@ define(['../animation_base', '../controls'], function(animation, controls) {
             console.log('holes set to ' + val);
         }
     };
+
+    var DIODE_WIDTH = 300;
+    var DIODE_HEIGHT = 30;
+    var CANVAS_WIDTH = 400;
+    var CANVAS_HEIGHT = 300;
+    var CANVAS_ELEVATION = 20;
+
+    var Drawable = function() {
+        var Drawable = {
+            Graphics: new PIXI.Graphics(),
+            draw: function() {}
+        }
+
+        return Drawable;
+    }
+
+    var CanvasRect = function() {
+        var CanvasRect = Drawable();
+
+        CanvasRect.draw = function() {
+            CanvasRect.Graphics.clear();
+
+            CanvasRect.Graphics.beginFill(0x00cc44);
+            CanvasRect.Graphics.drawRect(-CANVAS_WIDTH/2,-CANVAS_HEIGHT/2 - CANVAS_ELEVATION,CANVAS_WIDTH,CANVAS_HEIGHT);
+            CanvasRect.Graphics.endFill();
+        }
+
+        return CanvasRect;
+    }
+    canvasRect = CanvasRect();
+
+    var DiodeLines = function() {
+        var DiodeLines = Drawable();
+
+        DiodeLines.draw = function() {
+            DiodeLines.Graphics.clear();
+            DiodeLines.Graphics.lineStyle(1, 0x000000, 1);
+            DiodeLines.Graphics.moveTo(0, -DIODE_HEIGHT/2);
+            DiodeLines.Graphics.lineTo(0, DIODE_HEIGHT/2);
+            DiodeLines.Graphics.drawRect(-DIODE_WIDTH/2, -DIODE_HEIGHT/2, DIODE_WIDTH, DIODE_HEIGHT);
+        }
+        return DiodeLines;
+    }
+    diodeLines = DiodeLines();
+
+    var DiodeRects = function() {
+        var DiodeRects = Drawable();
+
+        DiodeRects.draw = function() {
+            DiodeRects.Graphics.clear();
+
+            DiodeRects.Graphics.beginFill(0xff0000);
+            DiodeRects.Graphics.drawRect(-DIODE_WIDTH/2, -DIODE_HEIGHT/2, DIODE_WIDTH/2*settings.fillRatio, DIODE_HEIGHT);
+            DiodeRects.Graphics.endFill();
+
+            DiodeRects.Graphics.beginFill(0x0000ff);
+            DiodeRects.Graphics.drawRect(DIODE_WIDTH/2*(1-settings.fillRatio), -DIODE_HEIGHT/2, (DIODE_WIDTH/2) - DIODE_WIDTH/2*(1-settings.fillRatio), DIODE_HEIGHT);
+            DiodeRects.Graphics.endFill();
+        }
+
+        return DiodeRects;
+    }
+    diodeRects = DiodeRects();
+
+    var Ground = function() {
+        var POSITION = {
+            x: 180.0,
+            y: 40.0
+        }
+        var WIDTH = 20.0;
+        var HEIGHT = 10.0;
+        var LINES = 4;
+
+        var Ground = Drawable();
+
+        Ground.draw = function() {
+            Ground.Graphics.clear();
+            Ground.Graphics.lineStyle(1, 0x000000, 1);
+
+            for (var i = 0; i < LINES; i++) {
+                var y = POSITION.y + HEIGHT / LINES * i - HEIGHT / 2;
+                var diff = WIDTH / 2.0 / LINES * i;
+
+                Ground.Graphics.moveTo(POSITION.x + diff - WIDTH / 2, y);
+                Ground.Graphics.lineTo(POSITION.x + WIDTH / 2 - diff, y);
+            }
+        }
+
+        return Ground;
+    }
+    ground = Ground();
+
+    animation.onLoad = function() {
+
+        canvasRect.draw();
+        diodeLines.draw();
+        diodeRects.draw();
+        ground.draw();
+
+        animation.scene.addChild(canvasRect.Graphics);
+        animation.scene.addChild(diodeRects.Graphics);
+        animation.scene.addChild(diodeLines.Graphics);
+        animation.scene.addChild(ground.Graphics);
+    }
+
+    animation.onRender = function() {
+
+    }
+
 
     animation.settings = [
         controls.Text('Voltage'),
